4b7a7375aa26d3cd86c32b850ede1cba950d2b55
maxlane <maxlane@umich.edu>
2022-05-10 15:38:33 -0400

    Initial Commit

---
+++ WTW/WTWApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTWApp.swift
+//  WTW
+//
+//  Created by Max Lane on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WTWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
